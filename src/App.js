db34ccf36b8f90f6989dad61b8b8a0cdc8dcc816
nitinchalopadho <nitin@chalopadho.com>
2019-07-24 16:29:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
+import LazyLoad from 'react-lazyload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Loading = () => (
+	<div>
+		<h5>Loading.....</h5>
+	</div>
+);
+
+const Post = ({ title, id, body }) => (
+	<div className="post">
+		<h3>{title}</h3>
+		<p>{body}</p>
+	</div>
+);
+
+function App () {
+	return (
+		<div className="App">
+			<div>
+				{data.map((post) => (
+					<LazyLoad key={post.id} placeholder={<Loading />}>
+						<Post key={post.id} {...post} />
+					</LazyLoad>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
